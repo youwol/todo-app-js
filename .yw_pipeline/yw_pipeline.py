1b95d1dc4ce2e13cc7703f4b30f35e4871b562bc
greinisch <greinisch@youwol.com>
2022-01-26 20:05:03 +0100

    :tada:

---
+++ .yw_pipeline/yw_pipeline.py
@@ -0,0 +1,51 @@
+from typing import List
+
+from youwol.environment.forward_declaration import YouwolEnvironment
+from youwol.environment.models import IPipelineFactory
+from youwol.environment.models_project import Artifact, Flow, Pipeline, PipelineStep, FileListing
+from youwol.pipelines.pipeline_typescript_weback_npm import PublishCdnRemoteStep, PublishCdnLocalStep
+from youwol_utils.context import Context
+from youwol_utils.utils_paths import parse_json
+
+
+class BuildStep(PipelineStep):
+    id: str = "build"
+    run: str = "echo 'Nothing to do'"
+    sources: FileListing = FileListing(
+        include=["index.html", "package.json", "style.css"]
+    )
+
+    artifacts: List[Artifact] = [
+        Artifact(
+            id='dist',
+            files=FileListing(
+                include=["index.html", "package.json", "style.css"]
+            )
+        )
+    ]
+
+
+class PipelineFactory(IPipelineFactory):
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    async def get(self, _env: YouwolEnvironment, _ctx: Context):
+        return Pipeline(
+            id="custom pipeline",
+            projectName=lambda path: parse_json(path / "package.json")["name"],
+            projectVersion=lambda path: parse_json(path / "package.json")["version"],
+            steps=[
+                BuildStep(),
+                PublishCdnLocalStep(packagedArtifacts=['dist']),
+                PublishCdnRemoteStep()
+            ],
+            flows=[
+                Flow(
+                    name="prod",
+                    dag=[
+                        "build > publish-local > publish-remote "
+                    ]
+                )
+            ]
+        )
